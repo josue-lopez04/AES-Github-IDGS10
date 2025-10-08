5a16a037c105f40b2267913d6ff728aebeec1c88
jos <jos@lap-23.local>
2025-10-07 21:03:05 -0600

    ci: actualiza pipeline de Node.js con Jest

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const hash = require('./testindex');
+
+test('Verificar hash', () => {
+  const message = "contraseña";
+  const hashtest = hash(message);
+  expect(hashtest).toEqual("edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5");
+});
